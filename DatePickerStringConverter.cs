76d8857c55dc3976bfa030083359dfb37b366641
jahager <jahager@cpp.edu>
2024-11-29 17:46:20 -0800

    ToDoItemPage DatePicker
    Use DatePicker to view and change Date, needs converter for binding

---
+++ DatePickerStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class DatePickerStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return DateTime.Parse((string)value!);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value!;
+            return date.ToString("MM/dd/yyyy");
+        }
+    }
+}
